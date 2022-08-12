b312dc1d9063da052edf95a076aaf564dc42cff6
unknown <irinarizvanova160@gmail.com>
2022-08-12 00:35:17 -0400

    price test

--- src/test/java/githubProject/PricingTest.java
+++ src/test/java/githubProject/PricingTest.java
@@ -1,18 +1,25 @@
 package githubProject;
 
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PricingTest {
+
+    @BeforeAll
+    static void configure(){
+        Configuration.browserSize = "1920x1080";
+    }
 
     @Test
     void searchPricing(){
         open("https://github.com");
         $(byText("Pricing")).hover();
-        $("[href= '/pricing#compare-features']").shouldHave(text("Compare plans"));
+        $(byText("Compare plans")).click();
+        $("h1.h2-mktg").shouldHave(text("Choose the plan that’s right for you."));
     }
 }
